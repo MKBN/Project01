1e036c3aab08d6d6fda581c6481fb434b2e685e1
First Last <mbnhpe@ip-172-31-18-66.ec2.internal>
2021-11-13 06:04:35 +0000

    new feature added to hello.py

--- hello.py
+++ hello.py
@@ -3,3 +3,5 @@ print("Hello from the beautiful world of python")
 print("Hello from the beautiful world of python")
 print("Hello from the beautiful world of python")
 print("Testing CI")
+print("adding new features to python")
+
