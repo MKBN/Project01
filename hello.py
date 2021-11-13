0fa0d40b77e63f1b5efc7701e5fd4330c4fcecf9
First Last <mbnhpe@ip-172-31-18-66.ec2.internal>
2021-11-13 05:44:14 +0000

    Initial commit

---
+++ hello.py
@@ -0,0 +1,4 @@
+print("Hello from the beautiful world of python")
+print("Hello from the beautiful world of python")
+print("Hello from the beautiful world of python")
+print("Hello from the beautiful world of python")
